9dbe5d67a320be68fc98a8598d7416dc0eb223f1
Philip <philipax@kth.se>
2023-07-15 21:55:55 +0200

    change of logo name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -49,7 +49,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            P.AXELSSON
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
