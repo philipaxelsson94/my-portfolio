fd059475850f2b1d53bc8715f1f10790bf9dd10b
Philip Axelsson <philipaxelaxelsson@gmail.com>
2024-08-29 22:53:24 +0200

    test

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ export default function About() {
                     fontWeight: 800,
                     color: 'white',
                     }}>
-                    I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.
+                    babababa I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.
 
 I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming
 
